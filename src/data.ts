dd06c68055f78f6c8344d1550548fe70a77ea9b4
R4P0N1KT <shinnatensei@gmail.com>
2023-05-17 13:26:47 -0300

    feat: adding a products array

---
+++ src/data.ts
@@ -0,0 +1,19 @@
+const products = [
+  {
+    name: "Primeiro produto exlusivo da loja.",
+    description: "Sobre o produto e porquê ele é exclusivo.",
+    src: "assets/sla1.jpg",
+  },
+  {
+    name: "Secundo produto exlusivo da loja.",
+    description: "Sobre o produto e porquê ele é exclusivo.",
+    src: "assets/sla2.jpg",
+  },
+  {
+    name: "Terceiro produto exlusivo da loja.",
+    description: "Sobre o produto e porquê ele é exclusivo.",
+    src: "assets/sla3.jpg",
+  },
+];
+
+export default products;
