60b96eb3e5a9ace334b767ce15c3730feb8dc8ef
R4P0N1KT <shinnatensei@gmail.com>
2023-05-17 13:22:36 -0300

    feat: adding the site name in navbar and import of navbar.css

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./navbar.css";
 
 const Navbar: React.FC = () => {
-  return <div className="Navbar-container">Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <h2>Joy Coffe</h2>
+    </div>
+  );
 };
 
 export default Navbar;
