7f9e0a5d18fb72d34b994fb7e81051e24e90d580
R4P0N1KT <shinnatensei@gmail.com>
2023-05-17 13:24:27 -0300

    feat: adding the carousel logic and imports of css and data

--- src/presentation/components/carousel/carousel.tsx
+++ src/presentation/components/carousel/carousel.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import products from "../../../data";
+import "./carousel.css";
 
 const ProductsCarousel: React.FC = () => {
-  return <div className="carousel-container"></div>;
+  const [product, setProduct] = React.useState(products[0]);
+  const [currentProduct, setCurrentProduct] = React.useState<number>(
+    products.values.length
+  );
+
+  const previousProduct = () => {
+    if (currentProduct == 0) {
+      setProduct(products[2]);
+      setCurrentProduct(2);
+    } else {
+      setProduct(products[currentProduct - 1]);
+      setCurrentProduct(currentProduct - 1);
+    }
+  };
+
+  const nextProduct = () => {
+    if (currentProduct == 2) {
+      setProduct(products[0]);
+      setCurrentProduct(0);
+    } else {
+      setProduct(products[currentProduct + 1]);
+      setCurrentProduct(currentProduct + 1);
+      console.log(currentProduct);
+    }
+  };
+
+  return (
+    <div className="carousel-products">
+      <div className="carousel-container">
+        <button onClick={() => previousProduct()}>Anterior</button>
+        <div className="carousel-product">
+          <img src={product?.src} alt="produto" />
+          <div className="carousel-product-text">
+            <h3>{product?.name}</h3>
+            <h2>{product?.description}</h2>
+          </div>
+        </div>
+        <button onClick={() => nextProduct()}>Próximo</button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsCarousel;
