fbc365f9efa68866512afc6c82427e1382397d4d
R4P0N1KT <shinnatensei@gmail.com>
2023-05-20 00:05:24 -0300

    feat: adding card component and your styles

---
+++ src/presentation/components/card/card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./card.css";
+import products from "../../../data";
+
+const Card: React.FC = () => {
+  return (
+    <section className="cards">
+      <h1>Nossos Produtos</h1>
+      <div className="card-container">
+        {products.map((product) => (
+          <div className="product-container">
+            <img src={product.src} alt={product.name} />
+            <h3 className="product-name">{product.name}</h3>
+            <h4 className="product-price">R$ {product.price},00</h4>
+            <p className="product-description">{product.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Card;
