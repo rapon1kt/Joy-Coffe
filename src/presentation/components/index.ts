094863cd20cc8af658267abb038b43d68c46c98d
R4P0N1KT <shinnatensei@gmail.com>
2023-05-20 00:05:54 -0300

    refactor: adding the exports of new components (card and features)

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,2 +1,4 @@
-export { default as Navbar } from "./navbar/navbar";
+export { default as Card } from "./card/card";
 export { default as ProductsCarousel } from "./carousel/carousel";
+export { default as Features } from "./features/features";
+export { default as Navbar } from "./navbar/navbar";
