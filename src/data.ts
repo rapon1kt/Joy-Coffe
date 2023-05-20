7fa79761faef3309e4ad551dbcc6a47f2d3690e2
R4P0N1KT <shinnatensei@gmail.com>
2023-05-20 00:07:28 -0300

    feat: adding the products in data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,18 +1,24 @@
 const products = [
   {
-    name: "Primeiro produto exlusivo da loja.",
-    description: "Sobre o produto e porquê ele é exclusivo.",
-    src: "assets/sla1.jpg",
+    name: "Café Gelado",
+    description:
+      "O café gelado é uma bebida preparada à base de café, misturado com alimento congelado (ingredientes de sua escolha).",
+    price: 10,
+    src: "assets/ice-coffe.jpg",
   },
   {
-    name: "Secundo produto exlusivo da loja.",
-    description: "Sobre o produto e porquê ele é exclusivo.",
-    src: "assets/sla2.jpg",
+    name: "Café Cremoso",
+    description:
+      "Preparado com creme de leite, chantilly e leite. Todos os ingredientes são colocados de acordo com a sua opção.",
+    price: 7,
+    src: "assets/coffe.jpg",
   },
   {
-    name: "Terceiro produto exlusivo da loja.",
-    description: "Sobre o produto e porquê ele é exclusivo.",
-    src: "assets/sla3.jpg",
+    name: "Café Expresso",
+    description:
+      "Preparado de forma rápida e bem feita, para você que não quer perder tempo.",
+    price: 5,
+    src: "assets/express-coffe.jpg",
   },
 ];
 
