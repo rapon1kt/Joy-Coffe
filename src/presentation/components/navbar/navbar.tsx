c9adfb427731f6ea3ddfb78644910c6ebd37b8cc
R4P0N1KT <shinnatensei@gmail.com>
2023-05-20 00:02:45 -0300

    refactor: changing the style of navbar and adding a icon to it

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -4,6 +4,7 @@ import "./navbar.css";
 const Navbar: React.FC = () => {
   return (
     <div className="navbar-container">
+      <i className="fa-solid fa-mug-hot fa-bounce"></i>
       <h2>Joy Coffe</h2>
     </div>
   );
