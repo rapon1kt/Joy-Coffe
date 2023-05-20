58d053c137b1fd1604cb963299e2c8e2acca91d0
R4P0N1KT <shinnatensei@gmail.com>
2023-05-20 00:04:28 -0300

    feat: adding features component and your styles

---
+++ src/presentation/components/features/features.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./features.css";
+
+const Features: React.FC = () => {
+  return (
+    <div className="features-container">
+      <div className="feature">
+        <i className="icon fa-solid fa-circle-check fa-4x"></i>
+        <h3>Preço acessível</h3>
+        <p>
+          Na nossa cafeteria ler e tomar um bom café sai com o melhor preço!
+        </p>
+      </div>
+      <div className="feature">
+        <i className="icon fa-solid fa-bullseye fa-4x"></i>
+        <h3>Ambiente</h3>
+        <p>Temos o ambiente ideal para você e seus livros!</p>
+      </div>
+      <div className="feature">
+        <i className="icon fa-solid fa-heart fa-4x"></i>
+        <h3>Variedade na leitura</h3>
+        <p>Caso não tenha livros possuimos os clássicos em nossa cafeteria!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
