fa8453215556a2e8023c0cce55811679326cc709
R4P0N1KT <shinnatensei@gmail.com>
2023-05-26 18:00:48 -0300

    feat: adding the location export folder

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,3 +2,4 @@ export { default as Card } from "./card/card";
 export { default as ProductsCarousel } from "./carousel/carousel";
 export { default as Features } from "./features/features";
 export { default as Navbar } from "./navbar/navbar";
+export { default as Location } from "./location/location";
