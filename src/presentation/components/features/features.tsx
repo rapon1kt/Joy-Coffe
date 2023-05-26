acec15f959ebf9c4621fdf7117d0a31fa2f8b692
R4P0N1KT <shinnatensei@gmail.com>
2023-05-26 18:01:53 -0300

    refactor: changing the structure of features and add a title

--- src/presentation/components/features/features.tsx
+++ src/presentation/components/features/features.tsx
@@ -4,22 +4,27 @@ import "./features.css";
 const Features: React.FC = () => {
   return (
     <div className="features-container">
-      <div className="feature">
-        <i className="icon fa-solid fa-circle-check fa-4x"></i>
-        <h3>Preço acessível</h3>
-        <p>
-          Na nossa cafeteria ler e tomar um bom café sai com o melhor preço!
-        </p>
-      </div>
-      <div className="feature">
-        <i className="icon fa-solid fa-bullseye fa-4x"></i>
-        <h3>Ambiente</h3>
-        <p>Temos o ambiente ideal para você e seus livros!</p>
-      </div>
-      <div className="feature">
-        <i className="icon fa-solid fa-heart fa-4x"></i>
-        <h3>Variedade na leitura</h3>
-        <p>Caso não tenha livros possuimos os clássicos em nossa cafeteria!</p>
+      <h1>Nossas vantagens!</h1>
+      <div className="features">
+        <div className="feature">
+          <i className="icon fa-solid fa-circle-check fa-4x"></i>
+          <h3>Preço acessível</h3>
+          <p>
+            Na nossa cafeteria ler e tomar um bom café sai com o melhor preço!
+          </p>
+        </div>
+        <div className="feature">
+          <i className="icon fa-solid fa-bullseye fa-4x"></i>
+          <h3>Ambiente</h3>
+          <p>Temos o ambiente ideal para você e seus livros!</p>
+        </div>
+        <div className="feature">
+          <i className="icon fa-solid fa-heart fa-4x"></i>
+          <h3>Variedade na leitura</h3>
+          <p>
+            Caso não tenha livros possuimos os clássicos em nossa cafeteria!
+          </p>
+        </div>
       </div>
     </div>
   );
