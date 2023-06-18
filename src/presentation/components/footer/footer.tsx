fec61adbc6026edacecfe058cb80fd19893eabe6
R4P0N1KT <shinnatensei@gmail.com>
2023-06-18 02:45:18 -0300

    feat: create footer component

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer">
+      <p>Made with ❤️ by raponi :)</p>
+    </div>
+  );
+};
+
+export default Footer;
