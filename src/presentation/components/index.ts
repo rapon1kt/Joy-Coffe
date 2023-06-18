cbb318f3bf9eeeaaec77ec2cdf9fa221adcf368f
R4P0N1KT <shinnatensei@gmail.com>
2023-06-18 02:45:54 -0300

    feat: add team and footer to export on index

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -3,4 +3,5 @@ export { default as ProductsCarousel } from "./carousel/carousel";
 export { default as Features } from "./features/features";
 export { default as Navbar } from "./navbar/navbar";
 export { default as Location } from "./location/location";
-export { default as Creators } from "./creators/creators";
+export { default as Team } from "./team/team";
+export { default as Footer } from "./footer/footer";
