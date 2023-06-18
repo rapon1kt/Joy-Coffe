6cae49ccff8d55360fea10c0b08083c453049491
R4P0N1KT <shinnatensei@gmail.com>
2023-06-17 22:52:58 -0300

    feat: create creators files and export from index

---
+++ src/presentation/components/creators/creators.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./creators.css";
+
+const Creators: React.FC = () => {
+  return <div>Creators</div>;
+};
+
+export default Creators;
--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -3,3 +3,4 @@ export { default as ProductsCarousel } from "./carousel/carousel";
 export { default as Features } from "./features/features";
 export { default as Navbar } from "./navbar/navbar";
 export { default as Location } from "./location/location";
+export { default as Creators } from "./creators/creators";
