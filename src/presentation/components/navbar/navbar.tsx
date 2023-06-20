17d986c94022e0985e2609d3b0c6a97559e54ec8
rapon1kt <raponikt@gmail.com>
2023-06-20 17:08:12 -0300

    feat: fixing the word "coffee"

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar: React.FC = () => {
-  return (
-    <div className="navbar-container">
-      <i className="fa-solid fa-mug-hot fa-bounce"></i>
-      <h2>Joy Coffe</h2>
-    </div>
-  );
+	return (
+		<div className="navbar-container">
+			<i className="fa-solid fa-mug-hot fa-bounce"></i>
+			<h2>Joy Coffee</h2>
+		</div>
+	);
 };
 
 export default Navbar;
