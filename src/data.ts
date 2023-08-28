2aa56253bda1c7d27123e01d1140626bc6451f0c
rapon1kt <shinnatensei@gmail.com>
2023-08-27 22:29:05 -0300

    delete: files and data of ex-colaborators

--- src/data.ts
+++ src/data.ts
@@ -1,77 +1,42 @@
 const products = [
-  {
-    name: "Café Gelado",
-    description:
-      "O café gelado é uma bebida preparada à base de café, misturado com alimento congelado (ingredientes de sua escolha).",
-    price: "R$ 10,00",
-    src: "assets/ice-coffe.jpg",
-  },
-  {
-    name: "Café Cremoso",
-    description:
-      "Preparado com creme de leite, chantilly e leite. Todos os ingredientes são colocados de acordo com a sua opção.",
-    price: "R$ 7,00",
-    src: "assets/coffe.jpg",
-  },
-  {
-    name: "Café Expresso",
-    description:
-      "Preparado de forma rápida e bem feita, para você que não quer perder tempo.",
-    price: "R$ 5,00",
-    src: "assets/express-coffe.jpg",
-  },
-  {
-    name: "Livros",
-    description:
-      "Acompanhe seu café com uma boa leitura, do livro que você desejar!",
-    price: "GRÁTIS!",
-    src: "assets/book.jpeg",
-  },
+	{
+		name: "Café Gelado",
+		description:
+			"O café gelado é uma bebida preparada à base de café, misturado com alimento congelado (ingredientes de sua escolha).",
+		price: "R$ 10,00",
+		src: "assets/ice-coffe.jpg",
+	},
+	{
+		name: "Café Cremoso",
+		description:
+			"Preparado com creme de leite, chantilly e leite. Todos os ingredientes são colocados de acordo com a sua opção.",
+		price: "R$ 7,00",
+		src: "assets/coffe.jpg",
+	},
+	{
+		name: "Café Expresso",
+		description:
+			"Preparado de forma rápida e bem feita, para você que não quer perder tempo.",
+		price: "R$ 5,00",
+		src: "assets/express-coffe.jpg",
+	},
+	{
+		name: "Livros",
+		description:
+			"Acompanhe seu café com uma boa leitura, do livro que você desejar!",
+		price: "GRÁTIS!",
+		src: "assets/book.jpeg",
+	},
 ];
 
 const data = [
-  {
-    name: "Gustavo Raponi",
-    role: "Programador",
-    image: "assets/creators/raponi.jpg",
-    instagram: "raponi_13",
-    age: 16,
-  },
-  {
-    name: "Anelize",
-    role: "Fundadora",
-    image: "assets/creators/anelize.jpg",
-    instagram: "lize_fernandes",
-    age: 17,
-  },
-  {
-    name: "Ana Clara",
-    role: "Co-Fundadora",
-    image: "assets/creators/ana.jpg",
-    instagram: "_ana__souzaa",
-    age: 16,
-  },
-  {
-    name: "Pátricia D.",
-    role: "Designer",
-    image: "assets/creators/paty.jpg",
-    instagram: "patym.oon",
-    age: 16,
-  },
-  {
-    name: "Letícia Oliveira",
-    role: "Designer",
-    image: "assets/creators/leticia.jpg",
-    instagram: "le.oalmeida",
-    age: 16,
-  },
-  {
-    name: "Ingrid Tenório",
-    role: "Investidora",
-    image: "assets/creators/ingrid.jpg",
-    instagram: "iingridtenorio",
-    age: 16,
-  },
+	{
+		name: "Gustavo Raponi",
+		role: "Programador",
+		image: "assets/creators/raponi.jpg",
+		instagram: "raponi_13",
+		age: 16,
+	},
 ];
 
 export { products, data };
